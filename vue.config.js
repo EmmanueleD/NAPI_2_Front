0b0379652d97409b33ff4af6191a30a135df7309
EmmanueleD <emmanuele.online@gmail.com>
2022-06-19 17:13:34 -0300

    dwa

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production' ? '/projects/guelcom-app' : '/',
-      chainWebpack: config => {
-            config.module
-              .rule('images')
-                .set('parser', {
-                  dataUrlCondition: {
-                    maxSize: 4 * 1024 // 4KiB
-                  }
-                })
-          }
 }
